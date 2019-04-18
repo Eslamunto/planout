815a31a77f1fe56da43da57338d6148a683f82e4
eslamunto <eslam.shala@gmail.com>
2019-04-18 15:23:03 +0200

    add files

--- alpha/ruby/test/plan_out/interpretter_test.rb
+++ alpha/ruby/test/plan_out/interpretter_test.rb
@@ -1,29 +1,40 @@
 require 'test/unit'
 require 'json'
+require_relative '../test_helper'
+require_relative '../../lib/plan_out/interpreter'
 
-class MyTest < Test::Unit::TestCase
-
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
-  def setup
-    # Do nothing
-    compiled = JSON.parse('
+module PlanOut
+  class MyTest < Minitest::Test
+    def setup
+      # Do nothing
+      @compiled = JSON.parse('
 {"op":"seq","seq":[{"op":"set","var":"group_size","value":{"choices":{"op":"array","values":[1,10]},"unit":{"op":"get","var":"userid"},"op":"uniformChoice"}},{"op":"set","var":"specific_goal","value":{"p":0.8,"unit":{"op":"get","var":"userid"},"op":"bernoulliTrial"}},{"op":"cond","cond":[{"if":{"op":"get","var":"specific_goal"},"then":{"op":"seq","seq":[{"op":"set","var":"ratings_per_user_goal","value":{"choices":{"op":"array","values":[8,16,32,64]},"unit":{"op":"get","var":"userid"},"op":"uniformChoice"}},{"op":"set","var":"ratings_goal","value":{"op":"product","values":[{"op":"get","var":"group_size"},{"op":"get","var":"ratings_per_user_goal"}]}}]}}]}]}
   ')
-    interpreter_salt = 'foo'
+      @interpreter_salt = 'foo'
+    end
 
-  end
+    def test_interpreter
 
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
+      @proc = Interpreter(@compiled, @interpreter_salt, {'userid'=> 123454})
+      @params = @proc.get_params
+      assert_equal(@proc.get_params.get('specific_goal'), 1)
+      assert_equal(@proc.get_params.get('ratings_goal'), 320)
+      # proc = Interpreter(
+      #     self.compiled, self.interpreter_salt, {'userid': 123454})
+      # params = proc.get_params()
+      # self.assertEqual(proc.get_params().get('specific_goal'), 1)
+      # self.assertEqual(proc.get_params().get('ratings_goal'), 320)
+    end
 
-  def teardown
-    # Do nothing
-  end
 
-  # Fake test
-  def test_fail
+    def teardown
+      # Do nothing
+    end
 
-    fail('Not implemented')
+    # Fake test
+    def test_fail
+
+      fail('Not implemented')
+    end
   end
 end
