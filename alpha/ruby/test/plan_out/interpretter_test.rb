98f5cdcd7236aca704d913ecac194ecd771bf2a5
eslamunto <eslam.shala@gmail.com>
2019-04-18 14:30:01 +0200

    add unit test setup

--- alpha/ruby/test/plan_out/interpretter_test.rb
+++ alpha/ruby/test/plan_out/interpretter_test.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'json'
 
 class MyTest < Test::Unit::TestCase
 
@@ -6,6 +7,11 @@ class MyTest < Test::Unit::TestCase
   # to set up fixture information.
   def setup
     # Do nothing
+    compiled = JSON.parse('
+{"op":"seq","seq":[{"op":"set","var":"group_size","value":{"choices":{"op":"array","values":[1,10]},"unit":{"op":"get","var":"userid"},"op":"uniformChoice"}},{"op":"set","var":"specific_goal","value":{"p":0.8,"unit":{"op":"get","var":"userid"},"op":"bernoulliTrial"}},{"op":"cond","cond":[{"if":{"op":"get","var":"specific_goal"},"then":{"op":"seq","seq":[{"op":"set","var":"ratings_per_user_goal","value":{"choices":{"op":"array","values":[8,16,32,64]},"unit":{"op":"get","var":"userid"},"op":"uniformChoice"}},{"op":"set","var":"ratings_goal","value":{"op":"product","values":[{"op":"get","var":"group_size"},{"op":"get","var":"ratings_per_user_goal"}]}}]}}]}]}
+  ')
+    interpreter_salt = 'foo'
+
   end
 
   # Called after every test method runs. Can be used to tear
